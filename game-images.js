8ebf3a0d79e4fc5021ed3ee4eaeef65148c97a03
Thái Toàn <thaitoan1304@gmail.com>
2026-06-29 14:04:58 +0700

    lan 13

--- game-images.js
+++ game-images.js
@@ -1,10 +1,10 @@
 window.gameImages = {
-  "valorant": "images/Valorant.jpg",
+  valorant: "images/Valorant.jpg",
   "genshin-impact": "images/genshin-impact.jpg",
-  "fortnite": "images/fortnite.jpg",
+  fortnite: "images/fortnite.jpg",
   "lien-quan": "images/lienquan.jpg",
   "lien-minh-huyen-thoai": "images/league.jpg",
   "ea-sports-fc-25": "images/ea-sports-fc-25.jpg",
-  "minecraft": "images/minecraft.jpg",
+  minecraft: "images/minecraft.jpg",
   "honkai-star-rail": "images/honkai-star-rail.jpg",
 };
